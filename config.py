2313e47437429e70040b0657f1f2e5600c3fe597
erikn1ce <117830353+erikn1ce@users.noreply.github.com>
2022-11-09 15:50:50 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+# Here place for some values using in project
+
+name = ''
+yandex_music_login = 'ermolaev.aric'
+spotify_login = ''
+password = ''
+bluetooth_mac_address = ''
+friends_photos_folder = ''
+emojis_folder = ''
